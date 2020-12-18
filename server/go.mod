5414b2768ef6186cd436e2ae2470ee09a73dfdb3
Alexander Chelovekov <achelove@cisco.com>
2020-12-18 19:22:40 +0300

    Initial

---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module tcpdemo2/server
+
+go 1.15
+
+require github.com/elastic/go-elasticsearch v0.0.0
---
+++ server/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/elastic/go-elasticsearch"
+)
+
+type Rib struct {
+	VersionStr          string        `json:"version_str"`
+	NodeIDStr           string        `json:"node_id_str"`
+	EncodingPath        string        `json:"encoding_path"`
+	CollectionID        int           `json:"collection_id"`
+	CollectionStartTime string        `json:"collection_start_time"`
+	CollectionEndTime   string        `json:"collection_end_time"`
+	MsgTimestamp        string        `json:"msg_timestamp"`
+	SubscriptionID      string        `json:"subscription_id"`
+	SensorGroupID       []interface{} `json:"sensor_group_id"`
+	DataSource          string        `json:"data_source"`
+	Data                []struct {
+		VrfName        string `json:"vrfName"`
+		Address        string `json:"address"`
+		MaskLen        int    `json:"maskLen"`
+		L3NextHopCount int    `json:"l3NextHopCount"`
+		EventType      string `json:"eventType"`
+		NextHop        []struct {
+			Address      string `json:"address"`
+			OutInterface string `json:"outInterface"`
+			VrfName      string `json:"vrfName"`
+			Owner        string `json:"owner"`
+			Preference   int    `json:"preference"`
+			Metric       int    `json:"metric"`
+			Tag          int    `json:"tag"`
+			SegmentID    int    `json:"segmentId"`
+			TunnelID     int    `json:"tunnelId"`
+			EncapType    string `json:"encapType"`
+			NhTypeFlags  int    `json:"nhTypeFlags"`
+		} `json:"nextHop"`
+	} `json:"data"`
+}
+
+func ribhandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		data, _ := ioutil.ReadAll(r.Body)
+
+		var response Rib
+		err := json.Unmarshal([]byte(data), &response)
+
+		if err != nil {
+			log.Print(err)
+		}
+
+		empJSON, err := json.MarshalIndent(response, "", "  ")
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		fmt.Println(string(empJSON))
+
+		cfg := elasticsearch.Config{
+			Addresses: []string{
+				"http://10.62.186.54:9200",
+			},
+		}
+
+		es, _ := elasticsearch.NewClient(cfg)
+
+		res, err := es.Index("golang-index", strings.NewReader(string(data)))
+		if err != nil {
+			log.Fatalf("ERROR: %s", err)
+		}
+		defer res.Body.Close()
+
+		log.Println(res)
+
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/network/rib", ribhandler)
+	http.ListenAndServe(":10000", nil)
+}
