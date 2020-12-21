4004b93dcf42afe5c89212451c66d1b8bcb01be4
Alexander Chelovekov <achelove@cisco.com>
2020-12-21 18:13:08 +0300

    Update

--- main.go
+++ main.go
@@ -9,23 +9,31 @@ import (
 	"reflect"
 )
 
-func printStructInfo(x interface{}) {
+func FlattenStruct(src interface{}, dst interface{}, baseIndex int) {
 	// typeOf wrapped value inside the Interface{}
-	t := reflect.TypeOf(x)
-	fmt.Printf("-----Kind - %v - ----\n", t.Kind())
-	if t.Kind() != reflect.Struct {
+	// r is an object with proper methods to each of supertype
+
+	tSrc := reflect.TypeOf(src)
+	tDst := reflect.TypeOf(dst)
+
+	vSrc := reflect.ValueOf(&src).Elem()
+	vDst := reflect.ValueOf(&dst).Elem() // returns value of the inteface()
+
+	if tSrc.Kind() != reflect.Slice {
+		nSrc := tSrc.NumField()
+		nDst := tDst.NumField()
+
+		for i := 0; i < nSrc-1; i++ {
+			v := vSrc.Field(i).Interface().(string)
+			vDst.Field(i).SetString(v)
+		}
+
+		fmt.Println(src, dst, nDst)
+
+	} else if tSrc.Kind() != reflect.Struct {
 		fmt.Printf("ERR: Not a struct value, expected struct value of 'kind' struct\n")
 		return
 	}
-
-	n := t.NumField()
-	fmt.Printf("struct of type %v has %v fields\n", t, n)
-
-	for i := 0; i < n; i++ {
-		tt := t.Field(i)
-		fmt.Printf("Field index: %v,\nname: %v,\ntype: %v\n", i, tt.Name, tt.Type)
-	}
-	fmt.Println("-------")
 }
 
 type Rib struct {
@@ -62,32 +70,32 @@ type Rib struct {
 }
 
 type RibGeneric struct {
-	VersionStr          string        `json:"version_str"`
-	NodeIDStr           string        `json:"node_id_str"`
-	EncodingPath        string        `json:"encoding_path"`
-	CollectionID        int           `json:"collection_id"`
-	CollectionStartTime string        `json:"collection_start_time"`
-	CollectionEndTime   string        `json:"collection_end_time"`
-	MsgTimestamp        string        `json:"msg_timestamp"`
-	SubscriptionID      string        `json:"subscription_id"`
-	SensorGroupID       []interface{} `json:"sensor_group_id"`
-	DataSource          string        `json:"data_source"`
-	VrfName             string        `json:"data.vrfName"`
-	Address             string        `json:"data.address"`
-	MaskLen             int           `json:"data.maskLen"`
-	L3NextHopCount      int           `json:"data.l3NextHopCount"`
-	EventType           string        `json:"data.eventType"`
-	NextHopAddress      string        `json:"data.nexthop.address"`
-	OutInterface        string        `json:"data.nexthop.outInterface"`
-	NextHopVrfName      string        `json:"data.nexthop.vrfName"`
-	Owner               string        `json:"data.nexthop.owner"`
-	Preference          int           `json:"data.nexthop.preference"`
-	Metric              int           `json:"data.nexthop.metric"`
-	Tag                 int           `json:"data.nexthop.tag"`
-	SegmentID           int           `json:"data.nexthop.segmentId"`
-	TunnelID            int           `json:"data.nexthop.tunnelId"`
-	EncapType           string        `json:"data.nexthop.encapType"`
-	NhTypeFlags         int           `json:"data.nexthop.nhTypeFlags"`
+	VersionStr                string        `json:"version_str"`
+	NodeIDStr                 string        `json:"node_id_str"`
+	EncodingPath              string        `json:"encoding_path"`
+	CollectionID              int           `json:"collection_id"`
+	CollectionStartTime       string        `json:"collection_start_time"`
+	CollectionEndTime         string        `json:"collection_end_time"`
+	MsgTimestamp              string        `json:"msg_timestamp"`
+	SubscriptionID            string        `json:"subscription_id"`
+	SensorGroupID             []interface{} `json:"sensor_group_id"`
+	DataSource                string        `json:"data_source"`
+	DataVrfName               string        `json:"data.vrfName"`
+	DataAddress               string        `json:"data.address"`
+	DataMaskLen               int           `json:"data.maskLen"`
+	DataL3NextHopCount        int           `json:"data.l3NextHopCount"`
+	DataNexhHopEventType      string        `json:"data.eventType"`
+	DataNexhHopNextHopAddress string        `json:"data.nexthop.address"`
+	DataNexhHopOutInterface   string        `json:"data.nexthop.outInterface"`
+	DataNexhHopNextHopVrfName string        `json:"data.nexthop.vrfName"`
+	DataNexhHopOwner          string        `json:"data.nexthop.owner"`
+	DataNexhHopPreference     int           `json:"data.nexthop.preference"`
+	DataNexhHopMetric         int           `json:"data.nexthop.metric"`
+	DataNexhHopTag            int           `json:"data.nexthop.tag"`
+	DataNexhHopSegmentID      int           `json:"data.nexthop.segmentId"`
+	DataNexhHopTunnelID       int           `json:"data.nexthop.tunnelId"`
+	DataNexhHopEncapType      string        `json:"data.nexthop.encapType"`
+	DataNexhHopNhTypeFlags    int           `json:"data.nexthop.nhTypeFlags"`
 }
 
 func ribhandler(w http.ResponseWriter, r *http.Request) {
@@ -103,13 +111,12 @@ func ribhandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Print(err)
 		}
-		printStructInfo(response)
-		for _, v := range response.Data {
-			printStructInfo(v)
-			for _, v := range v.NextHop {
-				printStructInfo(v)
-			}
-		}
+
+		var newRib RibGeneric
+
+		var index int = 0
+
+		FlattenStruct(response, newRib, index)
 	}
 }
 
