dd55222235188d58ac06fad27774a645166912ba
Alexander Chelovekov <achelove@cisco.com>
2020-12-21 22:28:27 +0300

    Update

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func FlattenStruct(src interface{}, dst interface{}, baseIndex int) {
 		fmt.Printf("tDst has %v fields\n", nDst)
 
 		for i := 0; i < nSrc; i++ {
-			fmt.Println(vSrc.Type().Field(i).Name)
+			fmt.Printf("%v %v", vSrc.Type().Field(i).Name, vSrc.Type().Field(i).Type)
 		}
 
 		sliceLen := vSrc.Field(nSrc - 1).Len()
