79ed5c5734e1d4e51c135c90ca513ce41f2c3cdb
Alexander Chelovekov <achelove@cisco.com>
2020-12-22 19:47:10 +0300

    Update

--- main.go
+++ main.go
@@ -16,7 +16,8 @@ func FlattenStruct(src map[string]interface{}, dst map[string]interface{}, prefi
 			} else if reflect.TypeOf(v).Kind() == reflect.Slice {
 				s := reflect.ValueOf(v)
 				for i := 0; i < s.Len(); i++ {
-					fmt.Printf("value: %v\n", s.Index(i).MapKeys())
+					v := s.Index(i)
+					fmt.Printf("value: %v\n", reflect.TypeOf(v).Kind())
 				}
 			}
 		}
