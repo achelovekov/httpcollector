f33a5ce9c10e397910edb158cdb68550b07eccc7
Alexander Chelovekov <achelove@cisco.com>
2020-12-24 18:46:40 +0300

    Update

--- main.go
+++ main.go
@@ -9,108 +9,54 @@ import (
 	"reflect"
 )
 
-func mapToSlice(src map[string]interface{}, key string) []interface{} {
-	sli := make([]interface{}, len(src))
-
-	var i int
-
-	for k, v := range src {
-		if k != key {
-			vType := reflect.ValueOf(v).Type().Kind()
-			switch vType {
-			case reflect.String:
-				val := make([]interface{}, 2)
-				val[0] = k
-				val[1] = v.(string)
-				sli[i] = val
-				i = i + 1
-			case reflect.Int64:
-				val := make([]interface{}, 2)
-				val[0] = k
-				val[1] = v.(int)
-				sli[i] = val
-				i = i + 1
-			case reflect.Float64:
-				val := make([]interface{}, 2)
-				val[0] = k
-				val[1] = v.(float64)
-				sli[i] = val
-				i = i + 1
-			}
-
-		}
+func PrettyPrint(src map[string]interface{}) {
+	empJSON, err := json.MarshalIndent(src, "", "  ")
+	if err != nil {
+		log.Fatalf(err.Error())
 	}
-
-	if reflect.ValueOf(src[key]).Len() == 0 {
-		sli[len(src)-1] = make([]interface{}, 2)
-	} else {
-		sli[len(src)-1] = []interface{}{key, src[key]}
-	}
-	nilIndexes := make([]int, 0)
-
-	for i, v := range sli {
-		if v == nil {
-			nilIndexes = append(nilIndexes, i)
-		}
-	}
-
-	var j int
-
-	for _, v := range nilIndexes {
-		sli = append(sli[:v-j], sli[v-j+1:]...)
-		j = j + 1
-	}
-
-	return sli
-
+	fmt.Printf("MarshalIndent function output %s\n", string(empJSON))
 }
 
-func Flatten(src interface{}, dst map[string]interface{}, keysSlice []string, prefix string) {
+func combineHeaders(src map[string]interface{}, pathIndex int, path []string) (map[string]interface{}, int) {
 
-	jsonMap := src.(map[string]interface{})
+	header := make(map[string]interface{})
 
-	if len(prefix) > 0 {
-		prefix = prefix + "."
-	}
-	if len(keysSlice) > 0 {
-
-		jsonSlice := mapToSlice(jsonMap, keysSlice[0])
-
-		for _, v := range jsonSlice {
-			key := reflect.ValueOf(v).Index(0).Interface().(string)
-			value := reflect.ValueOf(v).Index(1).Interface()
-
-			if reflect.ValueOf(value).Type().Kind() != reflect.Slice {
-				dst[prefix+key] = value
-			} else if reflect.ValueOf(key).Interface().(string) == keysSlice[0] {
-
-				prefix = prefix + key
-
-				keysSliceCopy := make([]string, len(keysSlice))
-				copy(keysSliceCopy, keysSlice)
-
-				keysSliceCopy = keysSliceCopy[1:]
-
-				for i := 0; i < reflect.ValueOf(value).Len(); i++ {
-					res := reflect.ValueOf(jsonMap[key]).Index(i).Interface()
-					Flatten(res, dst, keysSliceCopy, prefix)
-				}
-
+	for k, v := range src {
+		if reflect.ValueOf(v).Type().Kind() != reflect.Slice {
+			if pathIndex == 0 {
+				header[k] = reflect.ValueOf(v).Interface()
+			} else {
+				header[path[pathIndex-1]+"."+k] = reflect.ValueOf(v).Interface()
 			}
 		}
+	}
+
+	return header, pathIndex
+}
+
+func Flatten(src map[string]interface{}, path []string, pathIndex int, header map[string]interface{}) {
+
+	addHeader, pathIndex := combineHeaders(src, pathIndex, path)
+	newHeader := make(map[string]interface{})
+
+	for k, v := range addHeader {
+		newHeader[k] = v
+	}
+
+	for k, v := range header {
+		newHeader[k] = v
+	}
+
+	if pathIndex == len(path) {
+		PrettyPrint(newHeader)
+	} else if reflect.ValueOf(src[path[pathIndex]]).Len() == 0 {
+		newHeader[path[pathIndex]] = make([]interface{}, 0)
+		PrettyPrint(newHeader)
 	} else {
-		for k, v := range jsonMap {
-			if reflect.ValueOf(v).Type().Kind() != reflect.Slice {
-				dst[prefix+k] = v
-			}
+		for i := 0; i < reflect.ValueOf(src[path[pathIndex]]).Len(); i++ {
+			v := reflect.ValueOf(src[path[pathIndex]]).Index(i).Interface()
+			Flatten(v.(map[string]interface{}), path, pathIndex+1, newHeader)
 		}
-	}
-	if len(keysSlice) == 0 {
-		emp, err := json.MarshalIndent(dst, "", "  ")
-		if err != nil {
-			log.Fatalf(err.Error())
-		}
-		fmt.Printf("Flattened output: %s\n", string(emp))
 	}
 }
 
@@ -122,7 +68,6 @@ func ribhandler(w http.ResponseWriter, r *http.Request) {
 		data, _ := ioutil.ReadAll(r.Body)
 
 		src := make(map[string]interface{})
-		dst := make(map[string]interface{})
 		err := json.Unmarshal(data, &src)
 		if err != nil {
 			panic(err)
@@ -132,14 +77,19 @@ func ribhandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatalf(err.Error())
 		}
-		fmt.Printf("MarshalIndent funnction output %s\n", string(srcJSON))
+		fmt.Printf("MarshalIndent function output %s\n", string(srcJSON))
 
 		if err != nil {
 			log.Fatalf(err.Error())
 		}
-		keysSlice := []string{"data", "nextHop"}
 
-		Flatten(src, dst, keysSlice, "")
+		var pathIndex int
+
+		header := make(map[string]interface{})
+
+		path := []string{"data", "nextHop"}
+
+		Flatten(src, path, pathIndex, header)
 	}
 }
 
