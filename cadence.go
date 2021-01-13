ffce401dcd904ae75446f4ffc59197dc8421ef87
Alexander Chelovekov <achelove@cisco.com>
2021-01-13 13:48:43 +0300

    Update

--- cadence.go
+++ cadence.go
@@ -177,10 +177,10 @@ func main() {
 	}
 
 	postReqHandler := &postReqHandler{esClient: esClient}
-	//http.HandleFunc("/network/vxlan:sys/eps", postReqHandler.vxlanSysEpsHandler)
-	//http.HandleFunc("/network/vxlan:sys/bd", postReqHandler.vxlanSysBdHandler)
-	//http.HandleFunc("/network/interface:sys/intf", postReqHandler.vxlanSysIntfHandler)
-	//http.HandleFunc("/network/environment:sys/ch", postReqHandler.vxlanSysChHandler)
+	http.HandleFunc("/network/vxlan:sys/eps", postReqHandler.vxlanSysEpsHandler)
+	http.HandleFunc("/network/vxlan:sys/bd", postReqHandler.vxlanSysBdHandler)
+	http.HandleFunc("/network/interface:sys/intf", postReqHandler.vxlanSysIntfHandler)
+	http.HandleFunc("/network/environment:sys/ch", postReqHandler.vxlanSysChHandler)
 	http.HandleFunc("/network/resources:sys/proc", postReqHandler.vxlanSysProcHandler)
 	http.ListenAndServe(":11000", nil)
 
