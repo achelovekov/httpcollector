0f3f8c432749a37016ffd7b989428c344815c627
Alexander Chelovekov <achelove@cisco.com>
2021-01-25 16:28:34 +0300

    Update

--- cadence.go
+++ cadence.go
@@ -31,7 +31,7 @@ func flattenMap(esClient *es.Client, src map[string]interface{}, path [][]string
 	}
 
 	for index := range path[pathIndex] {
-		fmt.Printf("index - %v", index)
+		fmt.Printf("index - %v\n", index)
 		if _, ok := src[path[pathIndex][index]]; ok {
 			v := reflect.ValueOf(src[path[pathIndex][index]]).Interface().(map[string]interface{})
 			if v, ok := v["attributes"]; ok {
