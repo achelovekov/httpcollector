3e65aba9ca9e01f15a51027b2139ada14258dd11
Alexander Chelovekov <achelove@cisco.com>
2021-01-26 12:02:30 +0300

    Update

--- cadence.go
+++ cadence.go
@@ -192,7 +192,7 @@ func (prh *postReqHandler) vxlanSysIntfHandler(w http.ResponseWriter, r *http.Re
 }
 
 func (prh *postReqHandler) vxlanSysChHandler(w http.ResponseWriter, r *http.Request) {
-	var path = [][]string{{"eqptSupCSlot"}, {"eqptSupC"}, {"eqptCPU"}}
+	var path = [][]string{{"eqptSupCSlot", "eqptLCSlot"}, {"eqptSupC", "eqptLC"}, {"eqptCPU"}}
 	var enrichKeys = []string{"eqptSupC.operSt"}
 	worker(prh.esClient, r, path, prh.enrichmentMap, enrichKeys)
 }
