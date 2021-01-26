127ec3f48a39323d653a984dcb09e09ffed22a14
Alexander Chelovekov <achelove@cisco.com>
2021-01-26 16:37:47 +0300

    Update

--- events.go
+++ events.go
@@ -153,17 +153,17 @@ func worker(esClient *es.Client, r *http.Request, path []string) {
 		if err != nil {
 			panic(err)
 		}
-		/*
-			srcJSON, err := json.MarshalIndent(src, "", "  ")
-			if err != nil {
-				log.Fatalf(err.Error())
-			}
-			fmt.Printf("MarshalIndent function output %s\n", string(srcJSON))
 
-			if err != nil {
-				log.Fatalf(err.Error())
-			}
-		*/
+		srcJSON, err := json.MarshalIndent(src, "", "  ")
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		fmt.Printf("MarshalIndent function output %s\n", string(srcJSON))
+
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+
 		var pathIndex int
 
 		header := make(map[string]interface{})
