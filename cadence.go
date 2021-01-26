744b799c71bc4360b8eba96bd9231d67a8ea08bd
Alexander Chelovekov <achelove@cisco.com>
2021-01-26 10:57:08 +0300

    Update

--- cadence.go
+++ cadence.go
@@ -139,17 +139,17 @@ func worker(esClient *es.Client, r *http.Request, path [][]string, enrichmentMap
 		if err != nil {
 			panic(err)
 		}
+		/*
+			srcJSON, err := json.MarshalIndent(src, "", "  ")
+			if err != nil {
+				log.Fatalf(err.Error())
+			}
+			fmt.Printf("MarshalIndent function output %s\n", string(srcJSON))
 
-		srcJSON, err := json.MarshalIndent(src, "", "  ")
-		if err != nil {
-			log.Fatalf(err.Error())
-		}
-		fmt.Printf("MarshalIndent function output %s\n", string(srcJSON))
-
-		if err != nil {
-			log.Fatalf(err.Error())
-		}
-
+			if err != nil {
+				log.Fatalf(err.Error())
+			}
+		*/
 		var pathIndex int
 
 		newHeader := make(map[string]interface{})
